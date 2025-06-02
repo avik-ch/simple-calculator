8288bf5ce011adeee906a826266bd0ce1fe14484
avik-ch <181900631+avik-ch@users.noreply.github.com>
2025-06-01 19:45:59 -0500

    Add functionality to operators

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const divByZeroError = 'Nope';
+
 function add(a, b) {
     return a + b;
 }
@@ -20,28 +22,41 @@ function operate(a, b, operator) {
             return add(a, b);
         case "-":
             return subtract(a, b);
-        case "*":
+        case "x":
             return multiply(a, b);
         case "/":
             if (b === 0) {
-                return "Division by 0 error";
+                disableOperators = true;
+                return divByZeroError;
             }
             return divide(a, b);
     }
 }
 
 // Initial state
-let num1 = 0, num2 = 0, operator = '+';
+let repeatedOperator = true;
+let disableOperators = false;
+
+let num1 = 0, num2 = '', operator = '+';
+function resetValues() {
+    num1 = 0;
+    num2 = '';
+    operator = '+';
+}
+
 
 const screen = document.querySelector('.screen');
 function updateDisplay(input) {
-    let text = screen.textContent.trim();
-    if (input === '.') {
-        if (text.indexOf('.') !== -1) {
-            screen.textContent = text + '.';
-        }
+    if (num2 === '') {
+        screen.textContent = input;
+        num2 = Number(input);
     } else {
-        screen.textContent = Number(text + input);
+        let text = screen.textContent.trim();
+        if (input === '.' && text.indexOf('.') === -1) {
+            screen.textContent = text + '.';
+        } else {
+            screen.textContent = Number(text + input);
+        }
     }
 }
 
@@ -49,7 +64,46 @@ function updateDisplay(input) {
 const numberKeys = document.querySelectorAll('.number');
 numberKeys.forEach(key => {
     key.addEventListener('click', (e) => {
-        console.log("Clicked " + key.textContent);
+        if (disableOperators) {
+            disableOperators = false;
+            resetValues();
+        }
+        repeatedOperator = false;
         updateDisplay(key.textContent);
+    });
+});
+
+
+const allClear = document.querySelector('#all-clear');
+allClear.addEventListener('click', (e) => {
+    resetValues();
+    screen.textContent = 0;
+    disableOperators = false;
+});
+
+
+const delBtn = document.querySelector('#delete');
+delBtn.addEventListener('click', (e) => {
+    screen.textContent = screen.textContent.slice(0, -1);
+});
+
+
+const operatorButtons = document.querySelectorAll('.operator');
+operatorButtons.forEach(key => {
+    key.addEventListener('click', (e) => {
+        if (!disableOperators) {
+            if (!repeatedOperator) {
+                num2 = Number(screen.textContent);
+                num1 = operate(num1, num2, operator);
+                if (num1 === divByZeroError) {
+                    screen.textContent = divByZeroError;
+                } else {
+                    num2 = '';
+                    screen.textContent = num1;
+                }
+                repeatedOperator = true;
+            }
+            operator = key.textContent;
+        }
     });
 });
